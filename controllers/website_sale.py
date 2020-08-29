76deaf4f5ef89c4434ff48db2d11b0b899859896
skadogam <kadogams@student.s19.be>
2020-08-29 12:33:59 +0200

    add event date check before placing the order

--- controllers/website_sale.py
+++ controllers/website_sale.py
@@ -21,18 +21,25 @@ class WebsiteSaleEventSet(WebsiteSale):
 
     @http.route()
     def payment_transaction(self, **kwargs):
-        """Payment transaction override to check cart quantities before placing the order.
+        """Payment transaction override to check event availability and cart quantities before placing the order.
 
         """
         # print('WebsiteSaleEventSet')
         order = request.website.sale_get_order()
         values = []
         for line in order.order_line:
-            if line.event_set_ok and line.product_id.event_seats_availability == 'limited':
-                cart_qty = int(sum(order.order_line.filtered(lambda p: p.product_id.id == line.product_id.id).mapped('product_uom_qty')))
-                avl_qty = line.product_id.event_seats_available
-                if cart_qty > avl_qty:
-                    values.append(_('You ask for %s products but only %s is available') % (cart_qty, avl_qty if avl_qty > 0 else 0))
+            if line.event_set_ok:
+                if line.product_id.event_is_expired:
+                    values.append(_('The registration qqis not longer available'))
+                elif line.product_id.event_seats_availability == 'limited':
+                    cart_qty = int(sum(
+                        order.order_line.filtered(
+                            lambda p: p.product_id.id == line.product_id.id).mapped('product_uom_qty')
+                        )
+                    )
+                    avl_qty = line.product_id.event_seats_available
+                    if cart_qty > avl_qty:
+                        values.append(_('You ask for %s seats but only %s is available') % (cart_qty, avl_qty if avl_qty > 0 else 0))
         if values:
             raise ValidationError('. '.join(values) + '.')
         return super(WebsiteSaleEventSet, self).payment_transaction(**kwargs)
