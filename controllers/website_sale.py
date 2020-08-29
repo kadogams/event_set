1c12a9929e0e84435eab022fecbe346a924f0bfb
skadogam <kadogams@student.s19.be>
2020-08-29 11:16:00 +0200

    update website_sale controller

---
+++ controllers/website_sale.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+from odoo import http, _
+from odoo.http import request
+from odoo.exceptions import ValidationError
+
+
+class WebsiteSaleEventSet(WebsiteSale):
+    @http.route()
+    def cart_update(self, **kwargs):
+        """Cart update override to express checkout if the product is an Event Set and reproduce the behaviour when
+        buying an event ticket.
+
+        """
+        product_tmpl_id = kwargs.get('product_template_id')
+        if product_tmpl_id and request.env['product.template'].browse(int(product_tmpl_id)).event_set_ok:
+            kwargs.update(express=True)
+        return super(WebsiteSaleEventSet, self).cart_update(**kwargs)
+
+    @http.route()
+    def payment_transaction(self, **kwargs):
+        """Payment transaction override to check cart quantities before placing the order.
+
+        """
+        # print('WebsiteSaleEventSet')
+        order = request.website.sale_get_order()
+        values = []
+        for line in order.order_line:
+            if line.event_set_ok and line.product_id.event_seats_availability == 'limited':
+                cart_qty = int(sum(order.order_line.filtered(lambda p: p.product_id.id == line.product_id.id).mapped('product_uom_qty')))
+                avl_qty = line.product_id.event_seats_available
+                if cart_qty > avl_qty:
+                    values.append(_('You ask for %s products but only %s is available') % (cart_qty, avl_qty if avl_qty > 0 else 0))
+        if values:
+            raise ValidationError('. '.join(values) + '.')
+        return super(WebsiteSaleEventSet, self).payment_transaction(**kwargs)
---
+++ controllers/website_sale_variant.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import http
+from odoo.addons.website_sale.controllers.variant import WebsiteSaleVariantController
+
+
+class WebsiteSaleEventSetVariantController(WebsiteSaleVariantController):
+    @http.route()
+    def get_combination_info_website(self, product_template_id, product_id, combination, add_qty, **kw):
+        kw['context'] = kw.get('context', {})
+        kw['context'].update(website_sale_event_set_get_quantity=True)
+        return super(WebsiteSaleEventSetVariantController, self).get_combination_info_website(product_template_id, product_id, combination, add_qty, **kw)
