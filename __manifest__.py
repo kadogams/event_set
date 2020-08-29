b4e1f85e3a3f19058919f670422f0a25a2d69603
skadogam <kadogams@student.s19.be>
2020-08-29 09:58:35 +0200

    update manifest

--- __manifest__.py
+++ __manifest__.py
@@ -3,24 +3,23 @@
     'name': "event_set",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Event set""",
 
     'description': """
-        Long description of module's purpose
+        Allows to sell multiple event registrations as a set.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "kadogams",
+    'website': "https://github.com/kadogams",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Marketing/Events',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['website', 'event_sale'],
 
     # always loaded
     'data': [
